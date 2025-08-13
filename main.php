eeb47c0b5ffc870ff6ecb13fe739765d8bb97bf6
jiem10 <jugan.johnmark.bandico@gmail.com>
2025-08-13 02:17:25 +0000

    main php syntax

--- main.php
+++ main.php
@@ -1,6 +1,20 @@
 <?php
-$color = "red";
-echo "My car is " . $color . "<br>";
-echo "My house is " . $COLOR . "<br>";
-echo "My boat is " . $coLOR . "<br>";
+
+$name = "Jugan, John Mark";
+$Age = "19";
+$school = "grc";
+$course = "Bsit";
+$subject = "websys";
+$students = "yes";
+
+echo "My name is " . $name . "<br>";
+echo "My house is " . $age . "<br>";
+echo "My school is " . $school . "<br>";
+echo "course " . $course . "<br>";
+echo "subject " . $subject . "<br>";
+echo "students " . $students . "<br>";
+
 ?>
+
+
+
